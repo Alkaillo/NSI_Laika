a3da703baf07828b7f06c8cace339970cdce893f
Alkaillo <73753086+Alkaillo@users.noreply.github.com>
2021-10-01 10:36:05 +0200

    01/10/21

---
+++ Numeration/python/main.py
@@ -0,0 +1,25 @@
+#DS
+'''p = 0
+n = 3
+for k in range(n):
+    for y in range(k):
+        p = p+1
+    print(p)
+
+def calcul(a,b):
+    import math
+    return math.sqrt(a*a+b*b)
+print(calcul(3,4))
+
+total = 0
+i = int(input('Quel est le loyer de depart'))
+annees = 0
+
+while i < 850:
+    total = total + 12*i
+    i  =i*1.03
+    annees = annees + 1
+if i >= 850:
+    print("Le prix maximal du loyer a ete depasse en", annees, ' annees', "\n Vous avez paye au total", total, "euros")'''
+
+#Suite
