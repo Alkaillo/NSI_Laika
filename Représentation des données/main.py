3aad9d548de0505824b21dfa198477216292a05b
Alkaillo <73753086+Alkaillo@users.noreply.github.com>
2022-01-04 10:54:25 +0100

    Update main.py

--- Représentation des données/main.py
+++ Représentation des données/main.py
@@ -1,19 +1,18 @@
 import statistics
-#rectangle_list = [4.5, 6, 7.5]
-#def is_square():
-#    if rectangle_list[0]**2 + rectangle_list[1]**2 == rectangle_list[2]**2:
-#        is_square = True
-#        print(is_square)
-#    else:
-#      is_square = False
-#       print(is_square)
-#
-#is_square()
+def is_square(rectangle_list):
+    if rectangle_list[0]**2 + rectangle_list[1]**2 == rectangle_list[2]**2:
+        is_square = True
+        print(is_square)
+    else:
+      is_square = False
+       print(is_square)
 
-#def min_max_moy(a_list):
-#    maxi=max(a_list)
-#    mini=min(a_list)
-#    moy = statistics.mean(a_list)
-#    print(maxi,mini,moy)
-#
-#min_max_moy([7,9,71,6,27,89])
+is_square([4.5, 6, 7.5])
+
+def min_max_moy(a_list):
+    maxi=max(a_list)
+    mini=min(a_list)
+    moy = statistics.mean(a_list)
+    print(maxi,mini,moy)
+
+min_max_moy([7,9,71,6,27,89])
